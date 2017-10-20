7fa40466cc3391a98b90ccef47d22a95fbf3ab1a
andywhardy <andywhardy@gmail.com>
2017-10-20 09:15:38 +0100

    TXM-2287 library upgrade updated hmrctest and scalatestplus-play

--- project/FrontendBuild.scala
+++ project/FrontendBuild.scala
@@ -20,13 +20,12 @@ object FrontendBuild extends Build with MicroService {
   )
 
   def test(scope: String = "test") = Seq(
-    //"uk.gov.hmrc" %% "hmrctest" % "3.0.0" % scope,
-    "uk.gov.hmrc" %% "hmrctest" % "2.3.0" % scope,
+    "uk.gov.hmrc" %% "hmrctest" % "3.0.0" % scope,
     "org.scalatest" %% "scalatest" % "2.2.6" % scope,
     "org.pegdown" % "pegdown" % "1.6.0" % scope,
     "org.jsoup" % "jsoup" % "1.8.1" % scope,
     "com.typesafe.play" %% "play-test" % PlayVersion.current % scope,
-    "org.scalatestplus.play" %% "scalatestplus-play" % "1.5.1" % scope,
+    "org.scalatestplus.play" %% "scalatestplus-play" % "2.0.1" % scope,
     "com.pyruby" % "java-stub-server" % "0.14" % scope
   )
 

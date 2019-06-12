ebe9daf31b394386683e3dad812a5d1a0593d782
chris-hill <26716179+ChrisHillCap@users.noreply.github.com>
2019-06-12 15:59:06 +0100

    SAR-3730 - Implemented Welsh for Timeout Box +   Amended stub controller to get and put in v2

--- app/model/MessageConstants.scala
+++ app/model/MessageConstants.scala
@@ -4,14 +4,36 @@ object MessageConstants {
 
   sealed trait MessageConstants {
     def home: String
+    def timeoutSeconds: String
+    def timeoutMinutes: String
+    def timeoutMinute: String
+    def timeoutSignedOut: String
+    def timeoutSignYouOut: String
+    def timeoutResumeSession: String
+    def timeoutInactive: String
   }
 
   object EnglishMessageConstants extends MessageConstants {
     val home = "Home"
+    val timeoutSeconds = "seconds"
+    val timeoutMinutes = "minutes"
+    val timeoutMinute = "minute"
+    val timeoutSignedOut = "You're about to be signed out"
+    val timeoutSignYouOut = "For your security, we'll sign you out in"
+    val timeoutResumeSession = "Resume your session"
+    val timeoutInactive = "You've been inactive for a while."
+
   }
 
   object WelshMessageConstants extends MessageConstants {
     val home = "Cartref"
+    val timeoutSeconds = "eiliad"
+    val timeoutMinutes = "o funudau"
+    val timeoutMinute = "munud"
+    val timeoutSignedOut = "Rydych ar fin cael eich allgofnodi"
+    val timeoutSignYouOut = "Er eich diogelwch, byddwn yn eich allgofnodi cyn pen"
+    val timeoutResumeSession = "Ailddechrau eich sesiwn"
+    val timeoutInactive = "Rydych wedi bod yn anweithredol am sbel."
   }
 
 }
--- test/views/MainTemplateViewSpec.scala
+++ test/views/MainTemplateViewSpec.scala
@@ -21,7 +21,6 @@ class MainTemplateViewSpec extends ViewSpec {
   }
 
   implicit val testRequest: FakeRequest[AnyContentAsEmpty.type] = FakeRequest()
-  val messages = app.injector.instanceOf[MessagesApi]
   val testHtml = Html("")
 
   "MainTemplate" should {
@@ -60,6 +59,53 @@ class MainTemplateViewSpec extends ViewSpec {
           doc.getLinkHrefAsText("homeNavHref") shouldBe content.navHref
           doc.getALinkText("homeNavHref") shouldBe EnglishMessageConstants.home
           doc.getSpanAsText("phase-banner-content") shouldBe content.phaseBannerHtml
+        }
+      }
+
+      "timeout" should {
+        "should display correct text in Welsh mode" in {
+          val testPage = views.html.v2.main_template(
+            title = content.title,
+            journeyData = Some(testAppLevelJourneyConfigV2),
+            welshEnabled = true
+          )(testHtml)
+          val doc: Document = Jsoup.parse(testPage.body)
+
+          doc.title shouldBe content.title
+          val textOfScript: String = doc.getElementById("timeoutScript").html()
+
+         textOfScript shouldBe
+            """$.timeoutDialog({timeout: 120, countdown: 30, """ +
+              """time: "eiliad", """ +
+              """title: "Rydych ar fin cael eich allgofnodi", """ +
+              """message: "Er eich diogelwch, byddwn yn eich allgofnodi cyn pen", """ +
+              """minute_text: "munud", """ +
+              """minutes_text: "o funudau", """ +
+              """keep_alive_button_text: "Ailddechrau eich sesiwn", """ +
+              """heading_text: "Rydych wedi bod yn anweithredol am sbel.", """ +
+              """keep_alive_url: '/lookup-address/renewSession',logout_url: '/lookup-address/destroySession?timeoutUrl=testTimeoutUrl'});var dialogOpen;"""
+        }
+
+        "should display correct text in english mode" in {
+          val testPage = views.html.v2.main_template(
+            title = content.title,
+            journeyData = Some(testAppLevelJourneyConfigV2)
+          )(testHtml)
+          val doc: Document = Jsoup.parse(testPage.body)
+
+          doc.title shouldBe content.title
+          val textOfScript: String = doc.getElementById("timeoutScript").html()
+
+          textOfScript shouldBe
+            """$.timeoutDialog({timeout: 120, countdown: 30, """ +
+            """time: "seconds", """ +
+             """title: "You're about to be signed out", """ +
+            """message: "For your security, we'll sign you out in", """ +
+            """minute_text: "minute", """ +
+            """minutes_text: "minutes", """ +
+            """keep_alive_button_text: "Resume your session", """ +
+            """heading_text: "You've been inactive for a while.", """ +
+              """keep_alive_url: '/lookup-address/renewSession',logout_url: '/lookup-address/destroySession?timeoutUrl=testTimeoutUrl'});var dialogOpen;"""
         }
       }
     }

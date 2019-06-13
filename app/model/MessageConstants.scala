4690a98548eddbab542b64f55a017d6d012b987b
brennaksamuel <42062758+brennaksamuel@users.noreply.github.com>
2019-06-13 16:55:54 +0100

    SAR-3932: Update Confirm page view to include Welsh

--- app/model/MessageConstants.scala
+++ app/model/MessageConstants.scala
@@ -4,6 +4,7 @@ object MessageConstants {
 
   sealed trait MessageConstants {
     def home: String
+    def back: String
     def timeoutSeconds: String
     def timeoutMinutes: String
     def timeoutMinute: String
@@ -11,10 +12,13 @@ object MessageConstants {
     def timeoutSignYouOut: String
     def timeoutResumeSession: String
     def timeoutInactive: String
+    def confirmSelectedAddressError1: String
+    def confirmSelectedAddressError2: String
   }
 
   object EnglishMessageConstants extends MessageConstants {
     val home = "Home"
+    val back = "Back"
     val timeoutSeconds = "seconds"
     val timeoutMinutes = "minutes"
     val timeoutMinute = "minute"
@@ -22,11 +26,13 @@ object MessageConstants {
     val timeoutSignYouOut = "For your security, we'll sign you out in"
     val timeoutResumeSession = "Resume your session"
     val timeoutInactive = "You've been inactive for a while."
-
+    val confirmSelectedAddressError1 = "You have not selected any address. Please"
+    val confirmSelectedAddressError2 = "search for and select your address"
   }
 
   object WelshMessageConstants extends MessageConstants {
     val home = "Cartref"
+    val back = "Yn ôl"
     val timeoutSeconds = "eiliad"
     val timeoutMinutes = "o funudau"
     val timeoutMinute = "munud"
@@ -34,6 +40,8 @@ object MessageConstants {
     val timeoutSignYouOut = "Er eich diogelwch, byddwn yn eich allgofnodi cyn pen"
     val timeoutResumeSession = "Ailddechrau eich sesiwn"
     val timeoutInactive = "Rydych wedi bod yn anweithredol am sbel."
+    val confirmSelectedAddressError1 = "Nid ydych wedi dewis unrhyw gyfeiriad."
+    val confirmSelectedAddressError2 = "Chwiliwch am eich cyfeiriad a’i ddewis"
   }
 
 }

306c30bb3be0c1767cca6173a26003b180a1b63d
Pete Smith <pete.smith@digital.hmrc.gov.uk>
2020-07-06 12:57:22 +0100

    [PS|SS] All unit tests passing

--- test/services/AddressLookupAddressServiceSpec.scala
+++ test/services/AddressLookupAddressServiceSpec.scala
@@ -2,17 +2,15 @@ package services
 
 import config.FrontendAppConfig
 import model.ProposedAddress
+import org.mockito.Matchers._
+import org.mockito.Mockito._
 import org.scalatest.concurrent.ScalaFutures
 import org.scalatestplus.mockito.MockitoSugar
+import org.scalatestplus.play.PlaySpec
 import org.scalatestplus.play.guice.GuiceOneAppPerSuite
-import org.scalatestplus.play.{OneAppPerSuite, PlaySpec}
-import play.api.libs.json.Json
-import services.AddressReputationFormats._
 import uk.gov.hmrc.address.v2._
-import uk.gov.hmrc.http.{HeaderCarrier, HttpResponse, Upstream5xxResponse}
-import uk.gov.hmrc.play.bootstrap.http.{DefaultHttpClient, HttpClient}
-import org.mockito.Mockito._
-import org.mockito.Matchers._
+import uk.gov.hmrc.http.HeaderCarrier
+import uk.gov.hmrc.play.bootstrap.http.HttpClient
 
 import scala.collection.immutable.Seq
 import scala.concurrent.Future
@@ -20,36 +18,15 @@ import scala.util.Random
 
 class AddressLookupAddressServiceSpec extends PlaySpec with GuiceOneAppPerSuite with MockitoSugar with ScalaFutures {
 
-  class Scenario(resp: Option[HttpResponse] = None) {
+  class Scenario(resp: List[AddressRecord]) {
     private implicit val ec = play.api.libs.concurrent.Execution.Implicits.defaultContext
     implicit val hc = HeaderCarrier()
     val end = "http://localhost:42"
+
     val httpClient = mock[HttpClient]
-
-    //    val get = new HttpGet with WSGet {
-    //
-    //      override def doGet(url: String)(implicit hc: HeaderCarrier) = {
-    //        if (!url.startsWith(s"$end/v2/uk/addresses?")) throw new IllegalArgumentException(s"Unexpected endpoint URL: $url")
-    //        resp match {
-    //          case Some(r) => Future.successful(r)
-    //          case None => throw new Upstream5xxResponse("Service unavailable", 503, 503)
-    //        }
-    //      }
-    //
-    //      override val hooks: Seq[HttpHook] = Seq.empty
-    //
-    //      override protected def configuration: Option[Config] = Some(Play.current.configuration.underlying)
-    //
-    //      override protected def actorSystem: ActorSystem = akka.actor.ActorSystem()
-    //    }
-
-
     val frontendAppConfig = app.injector.instanceOf[FrontendAppConfig]
-    //    val httpClient = app.injector.instanceOf[HttpClient]
-    when(httpClient.doGet(any(), any())(any(), any())).thenReturn(resp match {
-      case Some(r) => Future.successful(r)
-      case None => throw new Upstream5xxResponse("Service unavailable", 503, 503)
-    })
+
+    when(httpClient.GET[List[AddressRecord]](any(), any())(any(), any(), any())).thenReturn(Future.successful(resp))
 
     val service = new AddressLookupAddressService(frontendAppConfig, httpClient)(ec) {
       override val endpoint = end
@@ -58,40 +35,32 @@ class AddressLookupAddressServiceSpec extends PlaySpec with GuiceOneAppPerSuite 
 
   "find" should {
 
-    "find addresses by postcode & isukMode == false" in new Scenario(
-      resp = Some(HttpResponse(200, Some(Json.toJson(oneAddress))))
-    ) {
+    "find addresses by postcode & isukMode == false" in new Scenario(oneAddress) {
       service.find("ZZ11 1ZZ", isukMode = false).futureValue must be(toProposals(oneAddress))
     }
 
-    "map UK to GB & isukMode == false" in new Scenario(
-      resp = Some(HttpResponse(200, Some(Json.toJson(List(addr(Some("UK")))))))
-    ) {
+    "map UK to GB & isukMode == false" in new Scenario(List(addr(Some("UK")))) {
       service.find("ZZ11 1ZZ", isukMode = false).futureValue.head.country.code must be("GB")
     }
+
     "return multiple addresses with diverse country codes when isukMode == false" in new Scenario(
-      resp = Some(HttpResponse(200, Some(Json.toJson(
-        manyAddresses(0)(Some("foo")) ::: manyAddresses(1)(Some("UK"))))))
-    ) {
+      manyAddresses(0)(Some("foo")) ::: manyAddresses(1)(Some("UK"))) {
       service.find("ZZ11 1ZZ", isukMode = false).futureValue.map(a => a.country.code) mustBe Seq("foo", "GB", "GB")
     }
 
     "return no addresses where ukMode == true and all addresses are non UK addresses" in new Scenario(
-      resp = Some(HttpResponse(200, Some(Json.toJson(manyAddresses(2)(Some("foobar"))))))
-    ) {
+      manyAddresses(2)(Some("foobar"))) {
       service.find("ZZ11 1ZZ", isukMode = true).futureValue.headOption must be(None)
     }
     "return 2 addresses where ukMode == true and 2 out of 3 addresses are UK" in new Scenario(
-      resp = Some(HttpResponse(200, Some(Json.toJson(
-        manyAddresses(0)(Some("foo")) ::: manyAddresses(1)(Some("UK"))))))
-    ) {
+      manyAddresses(0)(Some("foo")) ::: manyAddresses(1)(Some("UK"))) {
 
       service.find("ZZ11 1ZZ", isukMode = true).futureValue.map(a => a.country.code) mustBe Seq("GB", "GB")
     }
   }
+
   private val manyAddresses = (numberOfAddresses: Int) =>
     (code: Option[String]) => someAddresses(numberOfAddresses, addr(code))
-
 
   private val oneAddress = someAddresses(1, addr(Some("GB")))
 

4d43bb72da8723a209f2261aff7bd07ccfde3986
Pete Smith <pete.smith@digital.hmrc.gov.uk>
2020-08-21 11:17:22 +0100

    [PS|SS] Removed references to MessageConstants in frontend error handler

--- app/config/AddressLookupFrontendErrorHandler.scala
+++ app/config/AddressLookupFrontendErrorHandler.scala
@@ -17,43 +17,34 @@
 package config
 
 import javax.inject.{Inject, Singleton}
-import play.api.i18n.MessagesApi
+import play.api.i18n.{Lang, Messages, MessagesApi}
 import play.api.mvc.Request
 import play.twirl.api.Html
 import uk.gov.hmrc.play.bootstrap.http.FrontendErrorHandler
-import model.MessageConstants.{EnglishMessageConstants => englishContent, WelshMessageConstants => welshContent}
 
 @Singleton
 class AddressLookupFrontendErrorHandler @Inject()(val messagesApi: MessagesApi,
                                                   frontendAppConfig: FrontendAppConfig) extends FrontendErrorHandler {
 
-  override def standardErrorTemplate(pageTitle: String, heading: String, message: String)(implicit rh: Request[_]): Html = {
-    val optWelshContentCookie = rh.cookies.get(ALFCookieNames.useWelsh)
-
-    val langSpecificMessages = optWelshContentCookie collect {
-      case welshCookie if welshCookie.value.toBoolean == true => welshContent(true)
-    } getOrElse (englishContent(true))
+  override def standardErrorTemplate(pageTitle: String, heading: String, message: String)(implicit request: Request[_]): Html = {
+    val messages = implicitly[Messages]
 
     views.html.error_template(
       appConfig = frontendAppConfig,
-      pageTitle = langSpecificMessages.intServerErrorTitle,
-      heading = langSpecificMessages.intServerErrorTitle,
-      message = langSpecificMessages.intServerErrorTryAgain
+      pageTitle = messages("constants.intServerErrorTitle"),
+      heading = messages("constants.intServerErrorTitle"),
+      message = messages("constants.intServerErrorTryAgain")
     )
   }
 
   override def notFoundTemplate(implicit request: Request[_]): Html = {
-    val optWelshContentCookie = request.cookies.get(ALFCookieNames.useWelsh)
-
-    val langSpecificMessages = optWelshContentCookie collect {
-      case welshCookie if welshCookie.value.toBoolean == true => welshContent(true)
-    } getOrElse(englishContent(true))
+    val messages = implicitly[Messages]
 
     views.html.error_template(
       appConfig = frontendAppConfig,
-      pageTitle = langSpecificMessages.notFoundErrorTitle,
-      heading = langSpecificMessages.notFoundErrorHeading,
-      message = langSpecificMessages.notFoundErrorBody
+      pageTitle = messages("constants.notFoundErrorTitle"),
+      heading = messages("constants.notFoundErrorHeading"),
+      message = messages("constants.notFoundErrorBody")
     )
   }
 

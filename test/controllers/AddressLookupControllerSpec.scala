c50a1026a519194ca7a8111071395cae84bb4213
Mark Collin <1205116+Ardesco@users.noreply.github.com>
2020-10-09 09:54:50 +0100

    #TAV-88 Fix broken test

--- test/controllers/AddressLookupControllerSpec.scala
+++ test/controllers/AddressLookupControllerSpec.scala
@@ -397,7 +397,7 @@ class AddressLookupControllerSpec
     ) {
       val res = controller.select("foo").apply(reqWelsh.withFormUrlEncodedBody("postcode" -> "ZZ11 1ZZ"))
       val html = contentAsString(res).asBodyFragment
-      html should include element withName("h1").withValue("Dewiswch cyfeiriad")
+      html should include element withName("h1").withValue("Dewiswch gyfeiriad")
       html should include element withName("input").withAttrValue("type", "radio").withAttrValue("name", "addressId").withAttrValue("value", "GB1234567890")
       html should include element withName("input").withAttrValue("type", "radio").withAttrValue("name", "addressId").withAttrValue("value", "GB1234567891")
       html should include element withName("button").withAttrValue("type", "submit").withValue("Yn eich blaen")

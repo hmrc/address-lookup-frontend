1e93859975d6cd99b038dcf6ef0f27a056288ea8
Pete Smith <pete.smith@digital.hmrc.gov.uk>
2020-10-28 10:56:40 +0000

    [PS|SS] TAV 161: Fix issue where sorting addresses was failing in specific cases

--- app/services/AddressService.scala
+++ app/services/AddressService.scala
@@ -16,22 +16,22 @@
 
 package services
 
-import javax.inject.{Inject, Singleton}
 import com.google.inject.ImplementedBy
 import config.FrontendAppConfig
 import forms.Postcode
+import javax.inject.{Inject, Singleton}
 import model.ProposedAddress
 import play.api.libs.json.{Json, OFormat}
 import services.AddressReputationFormats._
 import uk.gov.hmrc.address.v2._
+import uk.gov.hmrc.http.HeaderCarrier
+import uk.gov.hmrc.play.bootstrap.http.HttpClient
 
 import scala.concurrent.{ExecutionContext, Future}
-import uk.gov.hmrc.http.{HeaderCarrier, HttpGet}
-import uk.gov.hmrc.play.bootstrap.http.HttpClient
 
 @ImplementedBy(classOf[AddressLookupAddressService])
 trait AddressService {
-  def find(postcode: String, filter: Option[String] = None,isukMode:Boolean)(implicit hc: HeaderCarrier): Future[Seq[ProposedAddress]]
+  def find(postcode: String, filter: Option[String] = None, isukMode: Boolean)(implicit hc: HeaderCarrier): Future[Seq[ProposedAddress]]
 }
 
 @Singleton
@@ -39,7 +39,7 @@ class AddressLookupAddressService @Inject()(frontendAppConfig: FrontendAppConfig
 
   val endpoint = frontendAppConfig.addressReputationEndpoint
 
-  override def find(postcode: String, filter: Option[String] = None,isukMode:Boolean)(implicit hc: HeaderCarrier): Future[Seq[ProposedAddress]] = {
+  override def find(postcode: String, filter: Option[String] = None, isukMode: Boolean)(implicit hc: HeaderCarrier): Future[Seq[ProposedAddress]] = {
     http.GET[List[AddressRecord]](s"$endpoint/v2/uk/addresses", Seq("postcode" ->
       Postcode.cleanupPostcode(postcode).get.toString,
       "filter" -> filter.getOrElse(""))).map { found =>
@@ -53,41 +53,61 @@ class AddressLookupAddressService @Inject()(frontendAppConfig: FrontendAppConfig
           if ("UK" == addr.address.country.code) Country("GB", "United Kingdom")
           else addr.address.country
         )
-      }.filterNot( a => isukMode && a.country.code != "GB")
+      }.filterNot(a => isukMode && a.country.code != "GB")
 
-      sortAddresses(results)
+      results.sortWith(superCleverAlgorithm)
     }
   }
 
-  private def sortAddresses(proposedAddresses: Seq[ProposedAddress]) = proposedAddresses.sortWith((a, b) => {
+  private def superCleverAlgorithm(addressA: ProposedAddress, addressB: ProposedAddress) =
+    chooseComparisonMethod(addressA, addressB) match {
+      case CompareAddressesAsStrings(a, b) => a < b
+      case CompareAddressesWithOneNumber(a, b, a1, b1) =>
+        if (a1 != b1) a1 < b1 else a < b
+      case CompareAddressesWithTwoNumbers(a, b, a1, a2, b1, b2) =>
+        if (a2 != b2) a2 < b2
+        else if (a1 != b1) a1 < b1
+        else a < b
+      case CompareWhenOnlyOneAddressContainsANumber(aSortsBeforeB) => aSortsBeforeB
+    }
+
+  private def chooseComparisonMethod(a: ProposedAddress, b: ProposedAddress): AddressComparisonMethod = {
+    val pattern = "([0-9]+)".r
     val aString = a.lines.mkString(" ").toLowerCase()
     val bString = b.lines.mkString(" ").toLowerCase()
 
-    val pattern = "([0-9]+)".r
+    val numbersInA = pattern.findAllIn(aString).toSeq.take(2)
+    val numbersInB = pattern.findAllIn(bString).toSeq.take(2)
 
-    val d1 = pattern.findAllIn(aString).toSeq.take(2)
-    val d2 = pattern.findAllIn(bString).toSeq.take(2)
-
-    (d1, d2) match {
+    (numbersInA, numbersInB) match {
+      case (Seq(), Seq()) =>
+        CompareAddressesAsStrings(aString, bString)
       case (Seq(a1, a2), Seq(b1, b2)) =>
-        if (a2.toInt != b2.toInt) a2 < b2
-        else if (a1.toInt != b1.toInt) a1 < b1
-        else aString < bString
+        CompareAddressesWithTwoNumbers(aString, bString, a1.toInt, a2.toInt, b1.toInt, b2.toInt)
       case (sa, sb) if sa.nonEmpty && sb.nonEmpty =>
-        if (sa.last.toInt != sb.last.toInt) sa.last.toInt < sb.last.toInt
-        else aString < bString
-      case _ =>
-        aString < bString
+        CompareAddressesWithOneNumber(aString, bString, sa.last.toInt, sb.last.toInt)
+      case (sa, _) =>
+        // We only reach this case when either one side or the other contains one number, but not both
+        // We want the address containing a number to sort first, so if sa is nonempty we consider this a < b
+        CompareWhenOnlyOneAddressContainsANumber(sa.nonEmpty)
     }
-  })
+  }
+
+  trait AddressComparisonMethod
+
+  case class CompareAddressesWithTwoNumbers(a: String, b: String, a1: Int, a2: Int, b1: Int, b2: Int) extends AddressComparisonMethod
+
+  case class CompareAddressesWithOneNumber(a: String, b: String, a1: Int, b1: Int) extends AddressComparisonMethod
+
+  case class CompareAddressesAsStrings(a: String, b: String) extends AddressComparisonMethod
+
+  case class CompareWhenOnlyOneAddressContainsANumber(aSortsBeforeB: Boolean) extends AddressComparisonMethod
 }
 
 object AddressReputationFormats {
-
   implicit val format0: OFormat[Country] = Json.format[Country]
   implicit val format1: OFormat[LocalCustodian] = Json.format[LocalCustodian]
   implicit val format2: OFormat[Address] = Json.format[Address]
   implicit val format3: OFormat[AddressRecord] = Json.format[AddressRecord]
   implicit val format4: OFormat[International] = Json.format[International]
-
 }

058a19580d2ad8709190d6b9cc430d306081bf8d
Pete Smith <pete.smith@digital.hmrc.gov.uk>
2024-09-30 20:31:54 +1000

    [PS] CIR-619 - Drop exceptions from warn logs

--- app/services/WarnLoggingJavascriptErrorListener.scala
+++ app/services/WarnLoggingJavascriptErrorListener.scala
@@ -12,14 +12,14 @@ class WarnLoggingJavascriptErrorListener extends DefaultJavaScriptErrorListener 
   val logger = Logger("org.htmlunit.javascript.DefaultJavaScriptErrorListener")
 
   override def scriptException(page: HtmlPage, scriptException: ScriptException): Unit =
-    logger.warn("Error during JavaScript execution", scriptException)
+    logger.warn("Error during JavaScript execution")
 
   override def timeoutError(page: HtmlPage, allowedTime: Long, executionTime: Long): Unit =
     logger.warn("Timeout during JavaScript execution after " + executionTime + "ms; allowed only " + allowedTime + "ms")
 
   override def malformedScriptURL(page: HtmlPage, url: String, malformedURLException: MalformedURLException): Unit =
-    logger.warn("Unable to build URL for script src tag [" + url + "]", malformedURLException)
+    logger.warn("Unable to build URL for script src tag [" + url + "]")
 
   override def loadScriptError(page: HtmlPage, scriptUrl: URL, exception: Exception): Unit =
-    logger.warn("Error loading JavaScript from [" + scriptUrl + "].", exception)
+    logger.warn("Error loading JavaScript from [" + scriptUrl + "].")
 }

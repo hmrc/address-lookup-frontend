c4941b882cde859e6a51f864fad410df13fae0fd
MJCallahanPage <16954819+MJCallahanPage@users.noreply.github.com>
2025-12-17 09:38:53 +0000

    [CIR-2533] Make heading the Legend as per GDS pattern recommendation (#196)
    * [CIR-2533] Make heading the Legend as per GDS pattern recommendation
    
    * [CIR-2533] Dependency bumps

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -4,8 +4,8 @@ import sbt.*
 object AppDependencies {
 
   private val bootstrapPlayVersion = "10.4.0"
-  private val hmrcFrontendPlayVersion = "12.20.0"
-  private val hmrcMongoPlayVersion = "2.10.0"
+  private val hmrcFrontendPlayVersion = "12.24.0"
+  private val hmrcMongoPlayVersion = "2.11.0"
   private val jacksonVersion = "3.0.2"
 
   val compile: Seq[ModuleID] = Seq(
@@ -13,7 +13,7 @@ object AppDependencies {
     "uk.gov.hmrc"             %% "bootstrap-frontend-play-30"             % bootstrapPlayVersion,
     "uk.gov.hmrc"             %% "play-frontend-hmrc-play-30"             % hmrcFrontendPlayVersion,
     "uk.gov.hmrc.mongo"       %% "hmrc-mongo-play-30"                     % hmrcMongoPlayVersion,
-    "uk.gov.hmrc"             %% "play-conditional-form-mapping-play-30"  % "3.3.0",
+    "uk.gov.hmrc"             %% "play-conditional-form-mapping-play-30"  % "3.4.0",
     "com.github.tototoshi"    %% "scala-csv"                              % "2.0.0",
     "net.ruippeixotog"        %% "scala-scraper"                          % "3.2.0",
     "uk.gov.hmrc.objectstore" %% "object-store-client-play-30"            % "2.5.0"

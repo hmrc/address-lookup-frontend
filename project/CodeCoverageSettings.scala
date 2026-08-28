6c476502f94711fb4a9e31f744ae807d7001287e
grahampaulcook <11269301+grahampaulcook@users.noreply.github.com>
2026-08-28 08:05:21 +0100

    CIR-3188 Increase minimum code coverage

--- project/CodeCoverageSettings.scala
+++ project/CodeCoverageSettings.scala
@@ -16,7 +16,7 @@ object CodeCoverageSettings {
 
   val settings: Seq[Setting[?]] = Seq(
     ScoverageKeys.coverageExcludedPackages := excludedPackages.mkString(";"),
-    ScoverageKeys.coverageMinimumStmtTotal := 78,
+    ScoverageKeys.coverageMinimumStmtTotal := 84,
     ScoverageKeys.coverageFailOnMinimum := true,
     ScoverageKeys.coverageHighlighting := true
   )
